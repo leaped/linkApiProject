6e81f7a8339dc61d22e354067db2e9370424c382
Breno Martins <brenomsimao123@outlook.com>
2020-07-17 00:34:07 -0300

    Improvements on the application Config

--- Model/Database/crud.js
+++ Model/Database/crud.js
@@ -2,22 +2,8 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 //InsertDocuments - As parameters
-const insertDocuments = async function (db, callback, objetos) {
-    // Get the documents collection
-    const collection = db.collection('demo');
-    // Insert some documents
-    await collection.insertMany(objetos, function (err, result) {
-        if (err)
-            console.log(err)
-        callback(result);
-    });
-}
-
-//Find Documents - All
-const findDocuments = function (callback) {
-    const client = new MongoClient(process.env.database_CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true });
-    // Get the documents collection
-
+const insertDocuments = async function (callback, objetos) {
+    const client = new MongoClient(process.env.database_CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
     client.connect(async function (err) {
         if (err) {
             response.send({
@@ -27,14 +13,37 @@ const findDocuments = function (callback) {
             return
         }
 
-        const db = client.db("mongoDataBase")
-        const collection = db.collection('demo');
+        const db = client.db(process.env.database_NAME)
+        const collection = db.collection(process.env.database_COLLECTION)
+        await collection.insertMany(objetos, function (err, result) {
+            if (err)
+                console.log(err)
+            callback(result);
+        });
+    })
+}
+
+//Find Documents - All
+const findDocuments = function (callback) {
+    const client = new MongoClient(process.env.database_CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    client.connect(async function (err) {
+        if (err) {
+            response.send({
+                "Status": "Database Error",
+                "ErrorDescription": err
+            })
+            return
+        }
+
+        const db = client.db(process.env.database_NAME)
+        const collection = db.collection(process.env.database_COLLECTION)
         collection.find({}).toArray(function (err, docs) {
             callback(docs);
         });
     })
 }
 
+findDocuments(console.log)
 
 module.exports = {
     findDocuments,
--- ViewModel/refreshWonDeals.js
+++ ViewModel/refreshWonDeals.js
@@ -1,5 +1,3 @@
-
-const MongoClient = require('mongodb').MongoClient;
 const crud = require('../Model/Database/crud')
 const pipedrive = require('../Model/externalAPIs/pipedrive')
 const bling = require('../Model/externalAPIs/bling')
@@ -7,68 +5,54 @@ const date = new Date
 
 require('dotenv').config()
 
-const refreshWonDeals = function (request, response) {
-    const client = new MongoClient(process.env.database_CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(async function (err) {
-        if (err){            
-            response.send({
-                "Status": "Error",
-                "ErrorDescription": err
-            })
-            return
-        }
+const refreshWonDeals = async function (request, response) {
+    await pipedrive.filterDealsByStatus('won')
+        .then(async function (resp) {
+            let deals = resp.data.data.items
+            let count = 1
+            const logDatabase = []
+            deals.forEach(async function (element) {
+                const blingReturn = await bling.insertOrder(element.item.id + 'beta13', element.item.title, element.item.value)
+                    .then(response => {
+                        return response.data
+                    })
 
-        const db = client.db("mongoDataBase")
-        await pipedrive.filterDealsByStatus('won')
-            .then(async function (resp) {
-                let deals = resp.data.data.items
-                let count = 1
-                const logDatabase = []
-                deals.forEach(async function (element) {
-                    const blingReturn = await bling.insertOrder(element.item.id + 'beta12', element.item.title, element.item.value)
-                        .then(response => {
-                            return response.data
-                        })
+                if (blingReturn.retorno.pedidos) {
+                    const order = blingReturn.retorno.pedidos[0]
+                    logDatabase.push({
+                        registerDate: `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`,
+                        dealId: element.item.id,
+                        dealTitle: element.item.title,
+                        dealValue: element.item.value,
+                        orderID: order.pedido.idPedido
+                    })
+                } else {
+                    //Enable only if you desire to see in console if there are any deals with that problem
+                    //console.log("Already registered in BLING system")
+                }
 
-                    if (blingReturn.retorno.pedidos) {
-                        const order = blingReturn.retorno.pedidos[0]
-                        logDatabase.push({
-                            registerDate: `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`,
-                            dealId: element.item.id,
-                            dealTitle: element.item.title,
-                            dealValue: element.item.value,
-                            orderID: order.pedido.idPedido
-                        })
-                    } else {
-                        //Enable only if you desire to see if there are any deals with that problem
-                        //console.log("Already registered in BLING system")
-                    }
-
-                    if(count == deals.length){
-                        if (logDatabase.length != 0) {
-                            crud.insertDocuments(db, res => {
-                                response.send({
-                                    "Status": "Ok",
-                                    "data": res
-                                }) 
-                                client.close();
-                             }, logDatabase)
-                             return
-                        }else{
+                if (count == deals.length) {
+                    if (logDatabase.length != 0) {
+                        crud.insertDocuments(res => {
                             response.send({
-                                "Status": "Error",
-                                "ErrorDescription": "All deals are already registered in BLING system"
-                            })  
-                        }
-                        client.close();
-                    }                    
-                    count++
-                })
+                                "Status": "Ok",
+                                "data": res
+                            })
+                        }, logDatabase)
+                        return
+                    } else {
+                        response.send({
+                            "Status": "Error",
+                            "ErrorDescription": "All deals are already registered in BLING system"
+                        })
+                    }
+                }
+                count++
             })
-            .catch(e => {
-                console.log(`Error with Pipedrive: ${e}`)
-            })        
-    });
+        })
+        .catch(e => {
+            console.log(`Error with Pipedrive: ${e}`)
+        })
 }
 
 
